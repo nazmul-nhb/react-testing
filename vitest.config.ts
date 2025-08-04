2b0676dd7eaee8c9f8c9045f328d904068ac7355
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-08-04 23:45:23 +0600

    refactor(tests): reconfigured vites and jsdom configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		environment: 'jsdom',
-		// globals: true,
-		// setupFiles: 'tests/setup.ts',
+		globals: true,
+		setupFiles: '__tests__/setup.ts',
 	},
 });
